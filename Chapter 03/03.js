1a0fdec6c9b4748e0bf4dd7ca24421da049db3c7
cwy007 <chanweiyan007@gmail.com>
2018-12-13 23:09:36 +0800

    .is()

--- Chapter 03/03.js
+++ Chapter 03/03.js
@@ -6,12 +6,14 @@
 // See README.txt for more information.
 $(document).ready(function () {
   $('#switcher-default').addClass('selected');
-  $('#switcher button').click(function (event) {
-    var bodyClass = this.id.split('-')[1];
-    $('body').removeClass().addClass(bodyClass);
-    $('#switcher button').removeClass('selected');
-    $(this).addClass('selected');
-    event.stopPropagation();
+  $('#switcher').click(function (event) {
+    if ($(event.target).is('button')) {
+      var bodyClass = event.target.id.split('-')[1];
+      $('body').removeClass().addClass(bodyClass);
+      $('#switcher button').removeClass('selected');
+      $(this).addClass('selected');
+      event.stopPropagation();
+    }
   });
 
   $('#switcher').click(function (event) {
