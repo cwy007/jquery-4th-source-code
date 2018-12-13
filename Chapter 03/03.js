10301e88acdef35b1bc9f80f310ff665f0017881
cwy007 <chanweiyan007@gmail.com>
2018-12-13 22:48:44 +0800

    .stopPropagation()

--- Chapter 03/03.js
+++ Chapter 03/03.js
@@ -6,17 +6,16 @@
 // See README.txt for more information.
 $(document).ready(function () {
   $('#switcher-default').addClass('selected');
-  $('#switcher button').click(function () {
+  $('#switcher button').click(function (event) {
     var bodyClass = this.id.split('-')[1];
     $('body').removeClass().addClass(bodyClass);
     $('#switcher button').removeClass('selected');
     $(this).addClass('selected');
+    event.stopPropagation();
   });
 
   $('#switcher').click(function (event) {
-    if (event.target == this) {
-      $('#switcher button').toggleClass('hidden');
-    }
+    $('#switcher button').toggleClass('hidden');
   });
 
   $('#switcher h3').hover(function () {
