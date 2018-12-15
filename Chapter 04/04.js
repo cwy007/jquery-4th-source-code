1c01d6841715bf0294f5db14ff3d59595b81ae75
cwy007 <chanweiyan007@gmail.com>
2018-12-15 22:35:38 +0800

    animate()

--- Chapter 04/04.js
+++ Chapter 04/04.js
@@ -20,19 +20,36 @@ $(document).ready(function () {
       default:
         num = parseFloat(defaultSize);
     }
-    $speech.css('fontSize', num + 'px');
+    $speech.animate({
+      'fontSize': num + 'px'
+    }, 'slow');
   });
 
   var $secondPara = $('p').eq(1);
   $secondPara.hide();
   $('a.more').click(function (event) {
     event.preventDefault();
-    $secondPara.slideToggle('slow');
+    // $secondPara.slideToggle('slow');
+    $secondPara.animate({
+      opacity: 'toggle',
+      height: 'toggle'
+    }, 'slow');
     var $link = $(this);
     if ($link.text() == 'read more') {
       $link.text('read less');
     } else {
       $link.text('read more');
     }
+  });
+
+  $('div.label').click(function () {
+    var paraWidth = $('div.speech p').outerWidth();
+    var $switcher = $(this).parent();
+    var switcherWidth = $switcher.outerWidth();
+    $switcher.animate({
+      borderWidth: '5px',
+      left: paraWidth - switcherWidth,
+      height: '+=20px'
+    }, 'slow');
   });
 });
