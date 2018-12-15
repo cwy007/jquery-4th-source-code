b5c85cfd47f5790f7c405b44b15d1a404c49ab73
cwy007 <chanweiyan007@gmail.com>
2018-12-15 11:04:40 +0800

    $speech variable & parseFloat() function & switch case default syntax structure

--- Chapter 04/04.js
+++ Chapter 04/04.js
@@ -4,3 +4,22 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+$(document).ready(function () {
+  var $speech = $('div.speech');
+  var defaultSize = $speech.css('fontSize');
+
+  $('#switcher button').click(function () {
+    var num = parseFloat($speech.css('fontSize'));
+    switch (this.id) {
+      case 'switcher-large':
+        num *= 1.4;
+        break;
+      case 'switcher-small':
+        num /= 1.4;
+        break;
+      default:
+        num = parseFloat(defaultSize);
+    }
+    $speech.css('fontSize', num + 'px');
+  });
+});
