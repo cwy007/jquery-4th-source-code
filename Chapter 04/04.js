98d3ce4a2cf458201683831346db6863c4b3cd29
cwy007 <chanweiyan007@gmail.com>
2018-12-16 21:39:07 +0800

    animate queue

--- Chapter 04/04.js
+++ Chapter 04/04.js
@@ -46,12 +46,10 @@ $(document).ready(function () {
     var paraWidth = $('div.speech p').outerWidth();
     var $switcher = $(this).parent();
     var switcherWidth = $switcher.outerWidth();
-    $switcher.css({
-      position: 'relative'
-    }).animate({
-      borderWidth: '5px',
-      left: paraWidth - switcherWidth,
-      height: '+=20px'
-    }, 'slow');
+    $switcher
+      .css({ position: 'relative' })
+      .animate({ left: paraWidth - switcherWidth }, 'slow')
+      .animate({ height: '+=20px' }, 'slow')
+      .animate({ borderWidth: '5px' }, 'slow');
   });
 });
