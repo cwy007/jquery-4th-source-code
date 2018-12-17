50793d0c87f4e74021c2bcbe6110aadc9b17a8a8
cwy007 <chanweiyan007@gmail.com>
2018-12-17 20:24:18 +0800

    this context & text() method

--- Chapter 05/05.js
+++ Chapter 05/05.js
@@ -8,7 +8,9 @@
 $(document).ready(function () {
   $('div.chapter a[href*="wikipedia"]').attr({
     rel: 'external',
-    title: 'Learn more at Wikipedia',
+    title: function () {
+      return 'Learn more about ' + $(this).text() + ' at Wikipedia.';
+    },
     id: function (index, _oldValue) {
       return 'wikilink-' + index;
     }
