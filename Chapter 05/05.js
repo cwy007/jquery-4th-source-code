8f87cf80c6b0e21c02e4901ce215a2a3f9cdd398
cwy007 <chanweiyan007@gmail.com>
2018-12-17 21:16:25 +0800

    move html elements with insertBefore

--- Chapter 05/05.js
+++ Chapter 05/05.js
@@ -18,4 +18,6 @@ $(document).ready(function () {
 
   $('<a href="#top">back to top</a>').insertAfter('div.chapter p');
   $('<a id="top"></a>').prependTo('body');
+
+  $('span.footnote').insertBefore('#footer');
 });
