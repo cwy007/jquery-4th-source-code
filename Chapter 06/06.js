195e95f6c55c6820d94fbcb4f31003d2e21d2b7b
cwy007 <chanweiyan007@gmail.com>
2018-12-18 23:10:46 +0800

    $.getJson() and $.each()

--- Chapter 06/06.js
+++ Chapter 06/06.js
@@ -11,4 +11,21 @@ $(document).ready(function () {
     $('#dictionary').load('a.html');
     alert('Loaded!');
   });
+
+  $('#letter-b a').click(function () {
+    event.preventDefault();
+    $.getJSON('b.json', function (data) {
+      var html = '';
+      $.each(data, function (_entryIndex, entry) {
+        html += '<div class="entry">';
+        html += '<h3 class="term">' + entry.term + '</h3>';
+        html += '<div class="part">' + entry.part + '</div>';
+        html += '<div class="definition">';
+        html += entry.definition;
+        html += '</div>';
+        html += '</div>';
+      });
+      $('#dictionary').html(html);
+    });
+  });
 });
