f899590c2acac7647228b526b19e6ded93b1fabc
cwy007 <chanweiyan007@gmail.com>
2018-12-23 21:35:38 +0800

    total quantiry

--- Chapter 08/08.js
+++ Chapter 08/08.js
@@ -18,3 +18,16 @@
     return total;
   };
 })(jQuery);
+
+$(document).ready(function () {
+  var $inventory = $('#inventory tbody');
+  var quantities = $inventory.find('td:nth-child(2)')
+    .map(function (index, qty) {
+      return $(qty).text();
+    }).get();
+  // console.log(quantities);
+  // console.log(quantities.get());
+
+  var sum = $.sum(quantities);
+  $('#sum').find('td:nth-child(2)').text(sum);
+});
