52c43775e7af7840a6f1b028e75f3dd3cf155044
cwy007 <chanweiyan007@gmail.com>
2018-12-23 21:28:02 +0800

    Add $.sum() method

--- Chapter 08/08.js
+++ Chapter 08/08.js
@@ -4,3 +4,17 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+
+(function ($) {
+  $.sum = function (array) {
+    var total = 0;
+
+    $.each(array, function (index, value) {
+      value = $.trim(value);
+      value = parseFloat(value) || 0;
+
+      total += value;
+    });
+    return total;
+  };
+})(jQuery);
