1ad1f20afc669c1bad81cd8488e370a18b602e18
cwy007 <chanweiyan007@gmail.com>
2018-12-31 07:18:45 +0800

    removeClass() & addClass()

--- Chapter 09/09.js
+++ Chapter 09/09.js
@@ -4,3 +4,15 @@
 // page or follow along with the examples in the book.
 //
 // See README.txt for more information.
+
+$(document).ready(function () {
+
+  $('#topics a').click(function (event) {
+    event.preventDefault(); // 阻止默认行为
+    var topic = $(this).text();
+
+    $('#topics a.selected').removeClass('selected'); // 去掉主题中，链接上的 selected 类
+    $(this).addClass('selected'); // 为当前被点击元素添加 selected 类
+  });
+
+});
