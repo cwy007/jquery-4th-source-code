ae5fa5b652e76de7ced38265263e1e1754afc2d3
cwy007 <chanweiyan007@gmail.com>
2018-12-31 17:08:42 +0800

    event delegate scope

--- Chapter 10/10.js
+++ Chapter 10/10.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     }
   });
 
-  $('#gallery').on('mouseenter mouseleave', 'div.photo', function (event) {
+  $(document).on('mouseenter mouseleave', 'div.photo', function (event) {
     var $details = $(this).find('.details');
     if (event.type == 'mouseenter') {
       $details.fadeTo('fast', 0.7);
