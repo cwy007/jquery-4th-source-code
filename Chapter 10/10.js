8dec6c410a9d50a2705c487bbf49e0acc4a9078c
cwy007 <chanweiyan007@gmail.com>
2018-12-31 16:26:55 +0800

    more-photos link click event

--- Chapter 10/10.js
+++ Chapter 10/10.js
@@ -1,6 +1,21 @@
-// This is the custom JavaScript file referenced by index.html. You will notice
-// that this file is currently empty. By adding code to this empty file and
-// then viewing index.html in a browser, you can experiment with the example
-// page or follow along with the examples in the book.
-//
-// See README.txt for more information.
+$(document).ready(function () {
+  var pageNum = 1;
+  $('#more-photos').click(function (event) {
+    event.preventDefault();
+    var $link = $(this);
+    var url = $link.attr('href');
+
+    if (url) {
+      $.get(url, function (data) {
+        $('#gallery').append(data);
+      });
+
+      pageNum++;
+      if (pageNum < 20) {
+        $link.attr('href', 'pages/' + pageNum + '.html');
+      } else {
+        $link.remove();
+      }
+    }
+  });
+});
