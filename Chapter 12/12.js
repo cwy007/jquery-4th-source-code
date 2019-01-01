46e0de4262e0333e4d5740c5e81027df0c2c450c
cwy007 <chanweiyan007@gmail.com>
2019-01-01 12:07:01 +0800

    arr.sort(), $.trim(), $.fn.eq(), $.fn.append()

--- Chapter 12/12.js
+++ Chapter 12/12.js
@@ -7,4 +7,24 @@ $(document).ready(function () {
   $headers
     .wrapInner('<a href="#"></a>')
     .addClass('sort');
+
+  $headers.on('click', function (event) {
+    event.preventDefault();
+    var column = $(this).index();
+    var rows = $table1.find('tbody > tr').get(); // get() 没有指定参数，和指定参数时效果不一样
+    rows.sort(function (a, b) {
+      var keyA = $(a).children('td').eq(column).text(); // eq()
+      keyA = $.trim(keyA).toUpperCase(); // $.trim(), es5 中也包含这个方法 string.trim()
+      var keyB = $(b).children('td').eq(column).text();
+      keyB = $.trim(keyB).toUpperCase(); // toUpperCase() javascript 方法
+
+      if (keyA < keyB) return -1;
+      if (keyA > keyB) return 1;
+      return 0;
+    });
+
+    $.each(rows, function (index, row) {
+      $table1.children('tbody').append(row); // append()
+    });
+  });
 });
--- Chapter 12/listings/12.2.js
+++ Chapter 12/listings/12.2.js
@@ -1,29 +1,30 @@
 /************************************************
   Table 1: Sorting by parsing cell contents.
 ************************************************/
-$(document).ready(function() {
+$(document).ready(function () {
   var $table1 = $('#t-1');
   var $headers = $table1.find('thead th').slice(1);
   $headers
     .wrapInner('<a href="#"></a>')
     .addClass('sort');
 
-  $headers.on('click', function(event) {
+  $headers.on('click', function (event) {
     event.preventDefault();
     var column = $(this).index();
-    var rows = $table1.find('tbody > tr').get();
-    rows.sort(function(a, b) {
-      var keyA = $(a).children('td').eq(column).text();
-      keyA = $.trim(keyA).toUpperCase();
+    var rows = $table1.find('tbody > tr').get(); // get() 没有指定参数，和指定参数时效果不一样
+    rows.sort(function (a, b) {
+      var keyA = $(a).children('td').eq(column).text(); // eq()
+      keyA = $.trim(keyA).toUpperCase(); // $.trim(), es5 中也包含这个方法 string.trim()
       var keyB = $(b).children('td').eq(column).text();
-      keyB = $.trim(keyB).toUpperCase();
+      keyB = $.trim(keyB).toUpperCase(); // toUpperCase() javascript 方法
+
       if (keyA < keyB) return -1;
       if (keyA > keyB) return 1;
       return 0;
     });
 
-    $.each(rows, function(index, row) {
-      $table1.children('tbody').append(row);
+    $.each(rows, function (index, row) {
+      $table1.children('tbody').append(row); // append()
     });
   });
 });
