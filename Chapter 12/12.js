0df9afae2759ebc7deead3e6c24b4ee5107fbae3
cwy007 <chanweiyan007@gmail.com>
2019-01-01 11:42:28 +0800

    $.fn.slice(start, end), $.fn.wrapInner()

--- Chapter 12/12.js
+++ Chapter 12/12.js
@@ -1,6 +1,10 @@
-// This is the custom JavaScript file referenced by index.html. You will notice
-// that this file is currently empty. By adding code to this empty file and
-// then viewing index.html in a browser, you can experiment with the example
-// page or follow along with the examples in the book.
-//
-// See README.txt for more information.
+/************************************************
+  Table 1: Sorting by parsing cell contents.
+************************************************/
+$(document).ready(function () {
+  var $table1 = $('#t-1');
+  var $headers = $table1.find('thead th').slice(1);
+  $headers
+    .wrapInner('<a href="#"></a>')
+    .addClass('sort');
+});
--- Chapter 12/listings/12.1.js
+++ Chapter 12/listings/12.1.js
@@ -1,10 +1,10 @@
 /************************************************
   Table 1: Sorting by parsing cell contents.
 ************************************************/
-$(document).ready(function() {
+$(document).ready(function () {
   var $table1 = $('#t-1');
-  var $headers = $table1.find('thead th').slice(1);
+  var $headers = $table1.find('thead th').slice(1); // $.fn.slice(start, end), 从选中的元素集合中取出一个子集，第一个数字表示开始的索引
   $headers
-    .wrapInner('<a href="#"></a>')
-    .addClass('sort');
+    .wrapInner('<a href="#"></a>') // 用新的元素包裹住选中元素的内容
+    .addClass('sort'); // 给 th 添加一个类 sort，用以区分外观
 });
